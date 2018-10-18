4594fea740e3739def68240f74ca3acf36bf92df
Aleks <av591955@gmail.com>
2018-10-18 15:39:26 +0300

    not working admin panel

---
+++ mpbi_project/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MpbiProjectConfig(AppConfig):
+    name = 'mpbi_project'
---
+++ mpbi_project/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+from django.shortcuts import render
+from .forms import ContactForm
+
+
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            pass  # does nothing, just trigger the validation
+    else:
+        form = ContactForm()
+    return render(request, 'home.html', {'form': form})
+
+# def home(request):
+    # return render(request, 'home.html')
