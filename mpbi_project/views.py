97634ab294e87eded848a7fe33880f985b639b9b
Alex <al.orzh@gmail.com>
2018-10-18 17:37:27 +0300

    Add form submission, add Contact model, add requirements file, add messageg to homepage

--- mpbi_project/views.py
+++ mpbi_project/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-from django.shortcuts import render
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 from .forms import ContactForm
+from .models import Contact
 
 
+@csrf_exempt
 def home(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            pass  # does nothing, just trigger the validation
+            contact = Contact()
+            contact.name = form.cleaned_data['name']
+            contact.email = form.cleaned_data['email']
+            contact.message = form.cleaned_data['message']
+
+            contact.save()
+
+            messages.info(request, 'Thank you (TBD)')
+            form = ContactForm()  # make it clean again
+
     else:
         form = ContactForm()
     return render(request, 'home.html', {'form': form})
